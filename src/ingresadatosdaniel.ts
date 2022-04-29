63c2b7b89bfd18612c7e7f0db3b58a9ca702a9ea
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-04-29 09:13:16 -0300

    cambios en html

--- src/ingresadatosdaniel.ts
+++ src/ingresadatosdaniel.ts
@@ -2,7 +2,7 @@ let btnEnv = document.getElementById("btnEnviar");
 let dato = document.forms[0].dato;
 let rotulo = document.getElementById("rotulo");
 
-btnEnv.addEventListener("clic", () => {
+btnEnv.addEventListener("click", () => {
   console.log("La altura es",dato.value );
 })
 rotulo.innerHTML= "Indique la altura de la persona";

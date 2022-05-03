21b44a248e11f0630e181e3596fb70e45cabafdf
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-05-03 13:40:56 -0300

    ingreso mayordetres

--- src/ingresadatosdaniel.ts
+++ src/ingresadatosdaniel.ts
@@ -3,6 +3,7 @@ let dato = document.forms[0].dato;
 let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click", () => {
+
   console.log("La altura es",dato.value );
 })
 rotulo.innerHTML= "Indique la altura de la persona";
---
+++ src/mayordetres.ts
@@ -0,0 +1,24 @@
+
+
+
+ let btnEnv = document.getElementById("btnEnviar");
+ let dato1 = document.getElementById("dato1");
+ let dato2 = document.getElementById("dato2");
+ let dato3 = document.getElementById("dato3");
+
+ btnEnv.addEventListener("click", () => {
+
+let mayor1 : number = Number(dato1.value);
+let mayor2 : number = Number(dato2.value);
+let mayor3 : number = Number(dato3.value);
+if(mayor1 > mayor2 && mayor1 > mayor3) {
+  console.log("El numero " + mayor1 + " es el mayor");
+} else if(mayor2 > mayor1 && mayor2 > mayor3) {
+
+  console.log("El numero " + mayor2 +" es el mayor");
+} else if(mayor3 > mayor1 && mayor3 > mayor2) {
+
+  console.log("El numero " + mayor3 + " es el mayor");
+
+}
+ })

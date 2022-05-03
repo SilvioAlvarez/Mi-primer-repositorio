72c7182cc51b6bd09ebc08468f6f175549d8c267
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-05-02 21:37:44 -0300

    agregue eureka

---
+++ src/eureka.ts
@@ -0,0 +1,13 @@
+let contador : number = 1;
+let claveingresada : string
+
+while(contador <= 3 && claveingresada != "eureka") {
+  claveingresada = prompt("Introduzca la clave");
+  contador ++;
+  console.log("clave incorrecta");
+}
+if (claveingresada == "eureka") {
+  console.log("clave correcta");
+
+  
+} 

cb9735dff7294264346b563e4151dd81ccd35b15
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-05-05 13:56:52 -0300

    eureka cambios

--- eureka/src/eureka.ts
+++ eureka/src/eureka.ts
@@ -1,14 +1,17 @@
 let contador : number = 1;
 let claveingresada : string
-
+                
 while(contador <= 3 && claveingresada != "eureka") {
   claveingresada = prompt("Introduzca la clave");
   contador ++;
-  console.log("clave incorrecta");
-}
+ 
+} 
 if (claveingresada == "eureka") {
   console.log("clave correcta");
-
-  
-} 
-rotulo.innerHTML = ("Ingrese la clave");
+      
+}
+else
+{
+  prompt("Intente despues de 24 horas");
+  console.log("Intente despues de 24 horas");
+}
--- src/mayordetres.ts
+++ src/mayordetres.ts
@@ -1,38 +1,18 @@
-let rotulo = document.getElementById("rotulo");
-let rotulo1 = document.getElementById("rotulo1");
-let rotulo2 = document.getElementById("rotulo2");
-
- let btnEnv = document.getElementById("btnEnviar");
- let dato1 = document.getElementById("dato1");
- let dato2 = document.getElementById("dato2");
- let dato3 = document.getElementById("dato3");
-
- btnEnv.addEventListener("click", () => {
-
-let mayor1 : number = Number(dato1.value);
-let mayor2 : number = Number(dato2.value);
-let mayor3 : number = Number(dato3.value);
-if(mayor1 > mayor2 && mayor1 > mayor3) {
-  console.log("El numero " + mayor1 + " es el mayor");
-} else if(mayor2 > mayor1 && mayor2 > mayor3) {
-
-  console.log("El numero " + mayor2 +" es el mayor");
-} else if(mayor3 > mayor1 && mayor3 > mayor2) {
-
-  console.log("El numero " + mayor3 + " es el mayor");
-  }
-  else if(mayor1 == mayor2 && mayor2 == mayor3) {
-    console.log("todos los numeros son iguales");
-  
-  }
-  else if(mayor1 == mayor2 && mayor1 > mayor3){
-  console.log("Se repite dos veces el mayor que es: ", + mayor1);
-
+let contador : number = 1;
+let claveingresada : string
+                
+while(contador <= 3 && claveingresada != "eureka") {
+  claveingresada = prompt("Introduzca la clave");
+  contador ++;
+ 
+} 
+if (claveingresada == "eureka") {
+  console.log("clave correcta");
+      
 }
-  else if(mayor1 < mayor2 && mayor2 == mayor3){
-  console.log("Se repite dos veces el mayor que es: ", + mayor2);
-  }
- })
- rotulo.innerHTML = ("Ingrese el primer numero");
- rotulo1.innerHTML = ("Ingrese el segundo numero");
- rotulo2.innerHTML = ("Ingrese el tercer numero");
+else
+{
+  prompt("Intente despues de 24 horas");
+  console.log("Intente despues de 24 horas");
+}
+

e8c0ea2e953f98e7c256f48c15c023f63c351966
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-05-08 21:12:05 -0300

    pares y multiplos

---
+++ paresymultiplos/src/paresymultiplos.ts
@@ -0,0 +1,13 @@
+let resultado : string = "";
+
+
+
+
+
+for (let num :number = 1; num <=100; num++)
+if (num% 2===0 || num% 3===0){
+  resultado = resultado+" "+num;
+}
+  console.log("Los numeros pares y los multiplos de 3 son: ", resultado);
+
+ 
--- src/mayordetres.ts
+++ src/mayordetres.ts
@@ -1,22 +1,27 @@
-let btnEnv = document.getElementById("btnEnviar");
-let rotulo = document.getElementById("rotulo");
-let rotulo1 = document.getElementById("rotulo1");
-let dato = document.getElementById("dato");
-let dato1 = document.getElementById("dato1");
 
-btnEnv.addEventListener("click", () => {
+let resultado : string = "";
+let resul : string = "";
+
+let resultad : number = 0;
+let result : number = 0;
+
+for (let pares : number = 0; pares <= 100; pares++)
+switch (pares) {
+  case pares%2 ===0:
+  resultad = pares + pares;
+  resultado = resultad+" "+pares;
+    console.log("resultado ",resultado)
+    break;
+case pares%3 ===0:
+result = pares * pares;
+resul = result+" "+result;
+console.log("resultado ", resul)
+
+    break;
+}
 
 
-let base : number = Number(dato.value);
-let altura : number = Number(dato1.value);
-let hipotenusa : number = 0; 
-let resultado : number = 0;
 
 
-hipotenusa = (base ** 2) + altura ** 2;
 
-resultado = hipotenusa**0.5;
-console.log(" es ", resultado);
-});
-rotulo.innerHTML = ("base");
-rotulo1.innerHTML = ("altura");
+

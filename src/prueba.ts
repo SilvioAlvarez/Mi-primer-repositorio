dfce0e7f9ecd53d428257284875a919c422249fa
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-05-16 22:18:15 -0300

    sumavectores

--- src/prueba.ts
+++ src/prueba.ts
@@ -1,11 +1,42 @@
-let numeroIngresado : number = Number(prompt("Ingrese un numero"));
+/*let numeroIngresado : number = Number(prompt("Ingrese un numero"));
  
 while (numeroIngresado <= 0){
   numeroIngresado = Number(prompt("Ingrese un numero positivo"));
 
 }if (numeroIngresado%2===0){
-  console.log("El numero "+numeroIngresado+" es par");
+  console.log("El numero "+numeroIngresado+" es par");*/
 
-}else{
-  console.log("El numero "+numeroIngresado+" es impar");
+
+let numeroIngresado : number = Number(prompt("Ingrese un numero"));
+ 
+while (numeroIngresado <= 0){
+  numeroIngresado = Number(prompt("Ingrese un numero positivo"));
 }
+for(let divisor: number=1; divisor<=numeroIngresado; divisor++){
+
+}if (numeroIngresado%divisor===0){
+
+
+  console.log("El numero ",divisor);
+
+}
+
+/*let v1 : number[] = new Array(6);
+let v2 : number[] = new Array(6);
+let vsuma : number[] = new Array(6);
+let indice: number;
+
+for (indice = 0;indice<6;indice++){
+v1[indice]=Number(prompt("Ingrese el valor v1["+indice+"]"));
+  
+}
+for (indice = 0;indice<6;indice++){
+v2[indice]=Number(prompt("Ingrese el valor v1["+indice+"]"));
+  
+  }
+  for (indice = 0;indice<6;indice++){
+  
+  vsuma[indice]=v1[indice]+v2[indice];
+  
+  console.log("La suma es",vsuma[indice]);
+  }*/

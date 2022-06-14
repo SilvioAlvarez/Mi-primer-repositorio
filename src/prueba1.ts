4bcaf6e4ef7380dab4a39904c2bb2b4c22506b6d
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-06-13 23:06:26 -0300

    prueba

--- src/prueba1.ts
+++ src/prueba1.ts
@@ -1,83 +1,84 @@
-
-let dimensionArreglo:number[]=10;
-let comisionA:number[]=new Array(10);
-let comisionB:number[]=new Array(10);
-let comisionC:number[]= new Array(10); 
-
-comisionA = [8, 5, 7, 10, 9, 2, 4, 8, 6, 8];  
-comisionB = [3, 6, 8, 5, 4, 7, 7, 4, 6, 8];  
-comisionC = [7, 6, 9, 8, 9, 8, 5, 7, 10, 9];
-
-function mayorPromedio(cantidad:number){
-
-   let sumanotasA:number=0;
-   let sumanotasB:number=0;
-   let sumanotasC:number=0;
-
-let indice:number;
-let promedioA:number=0;
-let promedioB:number=0;
-let promedioC:number=0;
-
-    for(indice=0;indice<dimensionArreglo;indice++){ 
-    sumanotasA+=comisionA[indice];
-    sumanotasB+=comisionB[indice];
-    sumanotasC+=comisionC[indice];
+let nombresVendedor:string[]=["Camila","Franco","Sofía","Matías","Agustina"];
+let vendedor1 :number[]=[32558,36165,34240,39273,34360,21347,20869,21493,23062,31908,30369,30652];
+let vendedor2 :number[]=[27520,31480,24053,34823,32942,38477,23756,21413,26699,22045,25625,26855];
+let vendedor3 :number[]=[20584,33473,34300,24598,33955,24040,39173,25542,25105,26759,29790,36218];
+let vendedor4 :number[]=[27243,38774,21246,30691,24542,39771,31807,31641,20850,29837,37182,28006];
+let vendedor5 :number[]=[23334,32687,25217,26844,27033,35244,25702,25781,35525,34874,38842,20562];
+let mesesVenta :string[]=["Enero","Enero","Enero","Enero","Febrero","Febrero","Febrero","Febrero","Marzo","Marzo","Marzo","Marzo","Marzo"];
+let semanasVentas:number[]=[1,2,3,4,1,2,3,4,1,2,3,4];
+ let longitud:number=12; 
+let promedios:number[]=0;
+ 
   
-  } promedioA=sumanotasA/dimensionArreglo;
-   promedioB=sumanotasB/dimensionArreglo;
-   promedioC=sumanotasC/dimensionArreglo;
-   
-    if(promedioA>promedioB&&promedioA>promedioC){
-    
-    console.log("El promedio mayor es  "+promedioA+ " de la comision A");
-    
-}else if(promedioC>promedioB){
-console.log("El mayor promedio es "+promedioC+ " de la comision C");
 
-}else {
-console.log ("El mayor promedio es " +promedioB+ " de la comision B");
-}
+  function cargarArreglo(arreglo:number[],longitud:number){
+    let indice:number;
+    for(indice=0;indice<longitud;indice++){
+      arreglo[indice]=vendedor[indice];
+    }return arreglo;
+  };
 
-    if(promedioA<promedioB&&promedioA<promedioC){
-console.log("El promedio menor es  "+promedioC+ " de la comision C");
-}else if(promedioC<promedioB){
-console.log("El menor promedio es "+promedioA+ " de la comision A");
-}else {
-console.log ("El menor promedio es " +promedioB+ " de la comision B");
-}}
-function estanDesaprobados(cantidad:number[]){
-let indice:number;
-let desaprobadosA:number=0;
-let desaprobadosB:number=0;
-let desaprobadosC:number=0;
+  function calcularVentaMaxima(arreglo:number[],mes:string[],semanas:number[],cantidad:number):string{
+    let indice:number;
+  let maximaVenta:number=0;
+  let mesVenta:string="";
+  let semanaVenta:number;
+  let texto :string= "";
+      for(indice=0;indice<arreglo.length;indice++){
+        if(arreglo[indice]>maximaVenta)
+        maximaVenta=arreglo[indice];
+        mesVenta=mes[indice];
+        semanaVenta=semanas[indice];
+      }
+      texto=" tuvo su mayor venta en la semana numero " + mesVenta+  " el mes de: "+ semanaVenta+ " por pesos: "+ maximaVenta;
+      return texto;
+  }
+  
+  console.log("La vendedora "+ nombresVendedor[0] + calcularVentaMaxima(vendedor1,semanasVentas,mesesVenta));
+  console.log("El vendedor "+ nombresVendedor[1] +calcularVentaMaxima(vendedor2,semanasVentas,mesesVenta));
+  console.log("La vendedora "+ nombresVendedor[2] +calcularVentaMaxima(vendedor3,semanasVentas,mesesVenta));
+  console.log("El vendedor "+ nombresVendedor[3] +calcularVentaMaxima(vendedor4,semanasVentas,mesesVenta));
+  console.log("La vendedora "+ nombresVendedor[4] +calcularVentaMaxima(vendedor5,semanasVentas,mesesVenta));
 
-  for(indice=0;indice<dimensionArreglo;indice++){ 
-    if(comisionA[indice]<6){
-    desaprobadosA++;
-    }if(comisionB[indice]<6){
-    desaprobadosB++;
-    }if(comisionC[indice]<6){
-    desaprobadosC++;
-}}console.log("Desaprobados en la comisionA "+desaprobadosA+ " desaprobados en la comisionB " +desaprobadosB+ " desaprobados en la comisionC "+desaprobadosC);
-}
-function menorNota(cantidad:number[]){
-let minimanotaA:number=11;
-let minimanotaB:number=11;
-let minimanotaC:number=11;
-
-    for(let indice:number=0;indice<dimensionArreglo;indice++){
-      if (comisionA[indice]<minimanotaA){
-        minimanotaA=comisionA[indice];
-      
-      }if (comisionB[indice]<minimanotaB){
-        minimanotaB=comisionB[indice];
-      }if (comisionC[indice]<minimanotaC){
-        minimanotaC=comisionC[indice];
+function calcularVentaMinima(arreglo:number[],mes:string[],semanas:number[],cantidad:number):string{
+    let indice:number;
+  let minimaVenta:number=40000;
+  let mesVenta:string="";
+  let texto :string= "";
+  let semanaVenta:number=1;
+      for(indice=0;indice<arreglo.length;indice++){
+        if(arreglo[indice]<minimaVenta){
+        minimaVenta=arreglo[indice];
+        mesVenta=mes[indice];
+        semanaVenta=semanas[indice];
+      }
+      texto=" tuvo su menor venta en la semana "+ mesVenta+ " en el mes de: "+ semanaVenta+ " por pesos: "+ minimaVenta;
+      return texto;
       }}
-      console.log("La minima nota de la comisionA es " +minimanotaA+ " la de la comisionB es "+minimanotaB+ " y la de lacomsionC "+minimanotaC);
-}
-mayorPromedio();
-menorNota();
-estanDesaprobados();
+          
+   console.log("La vendedora "+ nombresVendedor[0] + calcularVentaMinima(vendedor1,semanasVentas,mesesVenta));
+  console.log("El vendedor "+ nombresVendedor[1] +calcularVentaMinima(vendedor2,semanasVentas,mesesVenta));
+  console.log("La vendedora "+ nombresVendedor[2] +calcularVentaMinima(vendedor3,semanasVentas,mesesVenta));
+  console.log("El vendedor "+ nombresVendedor[3] +calcularVentaMinima(vendedor4,semanasVentas,mesesVenta));
+  console.log("La vendedora "+ nombresVendedor[4] +calcularVentaMinima(vendedor5,semanasVentas,mesesVenta));
 
+function promediarPorTrimestre(arreglo:number[],suma: number[],dimension:number):number[]{
+  let sumaTotal:number=0;
+    let promedio:number=0;
+    let textos:string="";
+   let indice:number=0;
+for(indice=0;indice<arreglo.length;indice++){
+     sumaTotal+=arreglo[indice];
+}
+ promedio=sumaTotal/(longitud);
+ 
+
+textos=" su promedio mensual es "+ promedio+ "y trimestral "+promedio*3;
+
+return textos;
+}
+ console.log("La vendedora "+ nombresVendedor[0] +  promediarPorTrimestre(vendedor1,promedios));
+  console.log("El vendedor "+ nombresVendedor[1] + promediarPorTrimestre(vendedor2,promedios));
+  console.log("La vendedora "+ nombresVendedor[2] +promediarPorTrimestre(vendedor3,promedios));
+  console.log("El vendedor "+ nombresVendedor[3] + promediarPorTrimestre(vendedor4,promedios));
+  console.log("La vendedora "+ nombresVendedor[4] + promediarPorTrimestre(vendedor5,promedios));

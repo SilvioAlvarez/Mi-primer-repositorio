701eeb205921bc1cd02351d1f960e54cb650922b
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-06-27 02:47:26 -0300

    prueb

--- src/prueba1.ts
+++ src/prueba1.ts
@@ -1,16 +1,32 @@
 let cantidadDeProductos:number=Number(prompt("Ingrese la cantidad de productos"));
 let productos:string[]= new Array (cantidadDeProductos);
 let precioUnitario:number[]=new Array(cantidadDeProductos);
+let precioUnitario:number[]=new Array(cantidadDeProductos);
+let cantidad:number[]=new Array(cantidadDeProductos);
+
 let i:number;
 
+let precio:number[]=0;
+let suma:number=0;
+let cant:number=0;
+
+
 for(i=0;i<cantidadDeProductos;i++){
-productos[i]=String(prompt("Ingrese el producto"+i));
-}for(i=0;i<cantidadDeProductos;i++){
+let total: string=" ";
+
+productos[i]=String(prompt("Ingrese el producto"));
+cantidad[i]=Number(prompt("Cuantos productos "+productos[i]+ "desea comprar"));
+ 
 precioUnitario[i]=Number(prompt("Ingrese el precio para el producto "+productos[i]));
-let div:string;
-if (productos[i]===productos[i]){
-div++;
+
+ 
+ precio= cantidad[i]*precioUnitario[i];
+        suma += precio;
+    cant+= cantidad[i];
+
+ total= "Productos  "+cant+"  Total de la compra " +suma;
+ console.log(cantidad[i]+" "+productos[i]+ " Punit "+precioUnitario[i]+" Ptotal "+precio);}
+console.log(total);
+  
 
 
-}}
-console.log(div)
